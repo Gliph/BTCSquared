6970c086fde76ed080b6fd7b567c21ab61bdf6d6
Joakim Fernstad <joakim@gli.ph>
2013-05-18 21:08:18 -0700

    Initial Commit

---
+++ BTC2/BTC2AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BTC2AppDelegate.h
+//  BTC2
+//
+//  Created by Joakim Fernstad on 5/18/13.
+//  Copyright (c) 2013 Joakim Fernstad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BTC2AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BTC2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BTC2
+//
+//  Created by Joakim Fernstad on 5/18/13.
+//  Copyright (c) 2013 Joakim Fernstad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BTC2AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BTC2AppDelegate class]));
+    }
+}
