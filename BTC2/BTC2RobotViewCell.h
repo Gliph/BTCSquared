21a9ee364f7c8838e90db2322b35176e5fb19fa7
Joakim Fernstad <joakim@houghstuffapp.com>
2013-05-19 00:13:07 -0700

    Adding Robot army to collection view

---
+++ BTC2/BTC2RobotViewCell.h
@@ -0,0 +1,14 @@
+//
+//  BTC2RobotViewCell.h
+//  BTC2
+//
+//  Created by Joakim Fernstad on 5/18/13.
+//  Copyright (c) 2013 Joakim Fernstad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "BTC2RobotView.h"
+
+@interface BTC2RobotViewCell : UICollectionViewCell
+@property (nonatomic, strong) IBOutlet BTC2RobotView* roboView;
+@end
