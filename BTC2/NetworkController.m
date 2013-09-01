6ec5f8e60c538fdb3002e6f1f69c8ff5fb222d0e
Joakim Fernstad <joakim@gli.ph>
2013-09-01 12:47:41 -0700

    Added MIT license to code. Organized files.

--- BTC2/NetworkController.m
+++ BTC2/NetworkController.m
@@ -1,9 +1,32 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2013 Joakim Fernstad
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ **/
+
 //
 //  NetworkController.m
 //  BTC2
 //
 //  Created by Joakim Fernstad on 5/18/13.
-//  Copyright (c) 2013 Joakim Fernstad. All rights reserved.
+//  Copyright (c) 2013 Joakim Fernstad. Releases under the MIT License.
 //
 
 #import "NetworkController.h"
@@ -42,13 +65,11 @@
     [UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
 #endif
 
-//    DLog(@"+1");
 }
 +(void)decreaseActivity{
 
     if ([NetworkController instance].indicatorCounter > 0) {
         [NetworkController instance].indicatorCounter--;
-//        DLog(@"-1");
     }
 
     // Save from over decrasing, might be unnecessary
